ba545292dbf26ecb0dc28aa5dbf0180600852557
Jonathan Peralez <jperalez@gmail.com>
2016-09-15 20:18:25 -0700

    Done through Ch 4

---
+++ index.js
@@ -0,0 +1,41 @@
+/**
+ * Created by Jonathan on 9/15/2016.
+ */
+
+var fortune = require('./lib/fortune');
+
+var express = require('express');
+
+var app = express();
+
+var handlebars = require('express-handlebars')
+    .create({ defaultLayout: 'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.render('home');
+});
+
+app.get('/about', function(req, res){
+    res.render('about', {fortune: fortune.getFortune()});
+});
+
+app.use(function(req, res) {
+    res.status(404);
+    res.render('404');
+});
+
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500);
+    res.send('500');
+});
+
+app.listen(app.get('port'), function() {
+    console.log('express started on http://localhost:' + app.get('port') + '; press Ctrl+C to terminate.');
+});
